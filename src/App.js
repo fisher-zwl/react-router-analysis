621422cea604bf2c1882abb920f4afd19d26c813
zhengwenlong <1426034446@qq.com>
2020-05-27 23:14:54 +0800

    添加有效的history和router源码

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './view/Home';
+import Page1 from './view/Page1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+          <Route path="/page1" exact component={Page1} />
+          <Route path="/" exact component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
